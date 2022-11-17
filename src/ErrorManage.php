4cd387d1fb0af88150e5f403bfa94ebf9f099d32
kiendt <kiendt22@fsoft.com.vn>
2022-11-18 01:50:28 +0700

    init package

---
+++ src/ErrorManage.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Virutmath\LaravelErrorManage;
+
+use Illuminate\Http\Response;
+
+class ErrorManage
+{
+    protected $messages;
+    public function __construct($messages) {
+        $this->messages = $messages;
+    }
+
+    public function response($errID) {
+        $err = [
+            'level' => 'error',
+            'status' => 500,
+            'type' => 'InternalServerError',
+            'code' => '900000500000',
+            'internal_message' => 'ErrorID not found',
+            'message' => 'Internal Server Error',
+            'message_detail' => 'An unknown internal error occurred. If the error persists, please contact service provider to figure out what has happened and how to fix it.'
+        ];
+        if(isset($this->messages[$errID])) {
+            $err = $this->messages[$errID];
+        }
+        return new Response($this->formatError($err), $err['status']);
+    }
+
+    protected function formatError($err) {
+        return [
+            'error' => [
+                'code' => $err['code'],
+                'message' => $err['message_detail'],
+            ]
+        ];
+    }
+}
